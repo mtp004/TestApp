64f9139fb0eb06af2ffb6627bfdcb2bfc779541b
Tri Pham <tri.pham121@icloud.com>
2025-03-01 20:01:25 -0500

    Completed the UI in DetailView

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -9,24 +9,26 @@ import SwiftUI
 
 struct CardView: View {
 	 let scrum: DailyScrum
+
+
 	 var body: some View {
-		 VStack(alignment: .leading) {
-			 Text(scrum.title)
-				 .font(.headline)
-				 .accessibilityAddTraits(.isHeader)
-			 Spacer()
-			 HStack{
-				 Label("\(scrum.attendees.count)", systemImage: "person.3")
-					 .accessibilityLabel("\(scrum.attendees.count) attendees")
-				 Spacer()
-				 Label("\(scrum.lengthInMinutes) minutes", systemImage: "clock")
-					 .labelStyle(.trailingIcon)
-					 .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
-			 }
-			 .font(.caption)
-		 }
-		 .padding()
-		 .foregroundColor(scrum.theme.accentColor)
+		  VStack(alignment: .leading) {
+				Text(scrum.title)
+					 .font(.headline)
+					 .accessibilityAddTraits(.isHeader)
+				Spacer()
+				HStack {
+					 Label("\(scrum.attendees.count)", systemImage: "person.3")
+						  .accessibilityLabel("\(scrum.attendees.count) attendees")
+					 Spacer()
+					 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+						  .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+						  .labelStyle(.trailingIcon)
+				}
+				.font(.caption)
+		  }
+		  .padding()
+		  .foregroundColor(scrum.theme.accentColor)
 	 }
 }
 
--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -15,23 +15,28 @@ struct DetailView: View {
 				Label("Start Meeting", systemImage: "timer")
 					.font(.headline)
 					.foregroundColor(.accentColor)
+				HStack {
+					Label("Length", systemImage: "clock")
+					Spacer()
+					Text("\(scrum.lengthInMinutes) minutes")
+				}
+				.accessibilityElement(children: .combine)
+				HStack {
+					Label("Theme", systemImage: "paintpalette")
+					Spacer()
+					Text("\(scrum.theme.name)")
+						.padding(4)
+						.foregroundColor(scrum.theme.accentColor)
+						.background(scrum.theme.mainColor)
+						.cornerRadius(4)
+				}
+				.accessibilityElement(children: .combine)
 			}
-			HStack {
-				Label("Length", systemImage: "clock")
-				Spacer()
-				Text("\(scrum.lengthInMinutes) minutes")
+			Section(header: Text("Attendees")) {
+				ForEach(scrum.attendees) {
+					attendee in Label(attendee.name, systemImage: "person")
+				}
 			}
-			.accessibilityElement(children: .combine)
-			HStack {
-				Label("Theme", systemImage: "paintpalette")
-				Spacer()
-				Text("\(scrum.theme.name)")
-					.padding(4)
-					.foregroundColor(scrum.theme.accentColor)
-					.background(scrum.theme.mainColor)
-					.cornerRadius(4)
-			}
-			.accessibilityElement(children: .combine)
 		}
     }
 }
