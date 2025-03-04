b6023382f5cecf745eceb71e51d0a6a467e750ed
Tri Pham <tri.pham121@icloud.com>
2025-03-04 16:12:20 -0500

    Added UI to DetailView to render DetailEditView sheet

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -19,8 +19,10 @@ struct DetailEditView: View {
 					Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1){
 						Text("Length in minutes")
 					}
+					.accessibilityValue("\(scrum.lengthInMinutes) minutes")
 					Spacer()
 					Text("\(scrum.lengthInMinutes) minutes")
+						.accessibilityHidden(true)
 				}
 			}
 			Section(header: Text("Attendees")){
@@ -39,6 +41,7 @@ struct DetailEditView: View {
 						}
 					}) {
 						Image(systemName: "plus.circle.fill")
+							.accessibilityLabel("Add attendee")
 					}
 				}
 			}
