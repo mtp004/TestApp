f403cf7f58202094faa16d2ae3111ce1f729d386
Tri Pham <tri.pham121@icloud.com>
2025-03-04 22:43:23 -0500

    Added editinf functionality to DetailView

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -1,78 +1,78 @@
-//
-//  DetailView.swift
-//  Scrumdinger
-//
-//  Created by Tri Pham on 2/27/25.
-//
-
 import SwiftUI
 
+
 struct DetailView: View {
-	let scrum: DailyScrum
+	 @Binding var scrum: DailyScrum
+
+
+	@State private var editingScrum = DailyScrum.emptyScrum
 	@State private var isPresentingEditView = false
-	
-	var body: some View {
-		List {
-			Section(header: Text("Meeting Info")) {
-				NavigationLink(destination: MeetingView()) {
-					Label("Start Meeting", systemImage: "timer")
-						.font(.headline)
-						.foregroundColor(.accentColor)
+	 
+	 var body: some View {
+		  List {
+				Section(header: Text("Meeting Info")) {
+					 NavigationLink(destination: MeetingView()) {
+						  Label("Start Meeting", systemImage: "timer")
+								.font(.headline)
+								.foregroundColor(.accentColor)
+					 }
+					 HStack {
+						  Label("Length", systemImage: "clock")
+						  Spacer()
+						  Text("\(scrum.lengthInMinutes) minutes")
+					 }
+					 .accessibilityElement(children: .combine)
+					 HStack {
+						  Label("Theme", systemImage: "paintpalette")
+						  Spacer()
+						  Text(scrum.theme.name)
+								.padding(4)
+								.foregroundColor(scrum.theme.accentColor)
+								.background(scrum.theme.mainColor)
+								.cornerRadius(4)
+					 }
+					 .accessibilityElement(children: .combine)
 				}
-				HStack {
-					Label("Length", systemImage: "clock")
-					Spacer()
-					Text("\(scrum.lengthInMinutes) minutes")
+				Section(header: Text("Attendees")) {
+					 ForEach(scrum.attendees) { attendee in
+						  Label(attendee.name, systemImage: "person")
+					 }
 				}
-				.accessibilityElement(children: .combine)
-				HStack {
-					Label("Theme", systemImage: "paintpalette")
-					Spacer()
-					Text("\(scrum.theme.name)")
-						.padding(4)
-						.foregroundColor(scrum.theme.accentColor)
-						.background(scrum.theme.mainColor)
-						.cornerRadius(4)
+		  }
+		  .navigationTitle(scrum.title)
+		  .toolbar {
+				Button("Edit") {
+					 isPresentingEditView = true
+					 editingScrum = scrum
 				}
-				.accessibilityElement(children: .combine)
-			}
-			Section(header: Text("Attendees")) {
-				ForEach(scrum.attendees) {
-					attendee in Label(attendee.name, systemImage: "person")
+		  }
+		  .sheet(isPresented: $isPresentingEditView) {
+				NavigationStack {
+					 DetailEditView(scrum: $editingScrum)
+						  .navigationTitle(scrum.title)
+						  .toolbar {
+								ToolbarItem(placement: .cancellationAction) {
+									 Button("Cancel") {
+										  isPresentingEditView = false
+									 }
+								}
+								ToolbarItem(placement: .confirmationAction) {
+									 Button("Done") {
+										  isPresentingEditView = false
+										  scrum = editingScrum
+									 }
+								}
+						  }
 				}
-			}
-		}
-		.navigationTitle(scrum.title)
-		.sheet(isPresented: $isPresentingEditView){
-			NavigationStack {
-				DetailEditView()
-					.navigationTitle(scrum.title)
-					.toolbar{
-						ToolbarItem(placement: .cancellationAction){
-							Button("Cancel"){
-								isPresentingEditView = false
-							}
-						}
-						ToolbarItem(placement: .confirmationAction){
-							Button("Done"){
-								isPresentingEditView = false
-							}
-						}
-					}
-			}
-		}
-		.toolbar{
-			Button("Edit"){
-				isPresentingEditView = true
-			}
-		}
-    }
+		  }
+	 }
 }
 
+
 struct DetailView_Previews: PreviewProvider {
-	static var previews: some View {
-		NavigationStack {
-			DetailView(scrum: DailyScrum.sampleData[0])
-		}
-	}
+	 static var previews: some View {
+		  NavigationStack {
+				DetailView(scrum: .constant(DailyScrum.sampleData[0]))
+		  }
+	 }
 }
