72e613f235957076b03c323bc6e5e04c92787f0b
Tri Pham <tri.pham121@icloud.com>
2025-03-10 15:57:27 -0400

    Added functionality to UI element in MeetingView: Timer now works, skipSpeaker button now works and update the current active speaker number

--- Scrumdinger/View/MeetingView.swift
+++ Scrumdinger/View/MeetingView.swift
@@ -2,26 +2,28 @@ import SwiftUI
 	 
 struct MeetingView: View {
 	@Binding var scrum: DailyScrum
+	@StateObject var scrumTimer = ScrumTimer()
 	
 	var body: some View {
 		ZStack {
 			RoundedRectangle(cornerRadius: 16).fill(scrum.theme.mainColor)
 			VStack {
+				MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed ,secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
 				Circle()
 					.strokeBorder(lineWidth: 24)
-				
-				HStack(){
-					Text("Speaker 1 of 3")
-					Spacer()
-					Button(action:{}){
-						Image(systemName: "forward.fill")
-					}
-				}
+				MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
 			}
 			.padding()
 		}
 		.foregroundColor(scrum.theme.accentColor)
 		.navigationBarTitleDisplayMode(.inline)
+		.onAppear{
+			scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+			scrumTimer.startScrum()
+		}
+		.onDisappear{
+			scrumTimer.stopScrum()
+		}
 	}
 }
 	 
